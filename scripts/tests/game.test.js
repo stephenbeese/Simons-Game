1ba9330fa1da82d4fb74f0e5e5c659f8abeefc58
stephenbeese <stephenbeese1@live.co.uk>
2023-03-22 12:51:56 +0000

    test addTurn function

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 
-const { game, newGame, showScore } = require("../game");
+const { game, newGame, showScore, addTurn } = require("../game");
 
 beforeAll(() => {
     let fs = require("fs");
@@ -43,8 +43,8 @@ describe("newGame works correctly", () => {
     test("should set game score to zero", () => {
         expect(game.score).toEqual(0);
     });
-    test("should clear the currentGame array", () => {
-        expect(game.currentGame.length).toBe(0);
+    test("should be one element in the computers array", () => {
+        expect(game.currentGame.length).toBe(1);
     });
     test("should clear the playerMoves array", () => {
         expect(game.playerMoves.length).toBe(0);
