72c10fd89dd8b7aa3c72e91e94afbc09e901e5e2
stephenbeese <stephenbeese1@live.co.uk>
2023-03-24 12:46:50 +0000

    Add playerTurn function to check playerMoves array against currentGame array

--- scripts/game.js
+++ scripts/game.js
@@ -1,77 +1,71 @@
-/** 
- *      JavaScript Structure
- * 
- *  game{object} - stores game state
- * 
- *  newGame()
- * 
- *  addturn()
- * 
- *  showTurns()
- * 
- *  lightsOn()
- * 
- *  playerTurn()
- * 
- *  showScore()
- * 
-*/
-
-
 let game = {
     score: 0,
     currentGame: [],
     playerMoves: [],
     turnNumber: 0,
-    choices: ["button1", "button2", "button3", "button4"],
+    choices: ["button1", "button2", "button3", "button4"]
 };
 
 function newGame() {
     game.score = 0;
     game.currentGame = [];
     game.playerMoves = [];
+
     for (let circle of document.getElementsByClassName("circle")) {
-        if (circle.getAttribute("data-listener")!== "true") {
+        if (circle.getAttribute("data-listener") !== "true") {
             circle.addEventListener("click", (e) => {
                 let move = e.target.getAttribute("id");
-                lightsOn(move);
                 game.playerMoves.push(move);
+                lightsOn(move);
                 playerTurn();
             });
             circle.setAttribute("data-listener", "true");
         }
-    };
+    }
     showScore();
     addTurn();
 }
 
-
 function addTurn() {
     game.playerMoves = [];
-    game.currentGame.push(game.choices[Math.floor(Math.random() * 4)]);
+    game.currentGame.push(game.choices[(Math.floor(Math.random() * 4))]);
     showTurns();
-}
-
-function showScore() {
-    document.getElementById("score").innerText = game.score
-}
-
-function lightsOn(circ) {
-    document.getElementById(circ).classList.add("light");
-    setTimeout(() => {
-        document.getElementById(circ).classList.remove("light");
-    }, 400);
 }
 
 function showTurns() {
     game.turnNumber = 0;
-    let turns = setInterval(() => {
+    let turns = setInterval(function () {
         lightsOn(game.currentGame[game.turnNumber]);
         game.turnNumber++;
-        if (game.turnnumber >= game.currentGame.length) {
+        if (game.turnNumber >= game.currentGame.length) {
             clearInterval(turns);
         }
-    }, 800)
+    }, 800);
 }
 
-module.exports = { game, newGame, showScore, addTurn, lightsOn, showTurns };
+function lightsOn(circ) {
+    document.getElementById(circ).classList.add('light');
+    setTimeout( function () {
+        document.getElementById(circ).classList.remove("light");
+    }, 400);
+}
+
+function playerTurn() {
+    let i = game.playerMoves.length - 1;
+    if (game.currentGame[i] === game.playerMoves[i]) {
+        if (game.currentGame.length === game.playerMoves.length) {
+            game.score++;
+            showScore();
+            addTurn();
+        } 
+    } else {
+        alert("Wrong move!");
+        newGame();
+    }
+}
+
+function showScore() {
+    document.getElementById("score").innerText = game.score;
+}
+
+module.exports = { game, newGame, showScore, addTurn, lightsOn, showTurns, playerTurn };
